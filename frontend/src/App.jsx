e38aebf683a719655f34bdac9f2463fdc4ffb0a6
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-04-12 02:23:56 +0530

    loader added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Report from './pages/Report';
+import LoaderNew from './components/LoaderNew';
+import Loader from './pages/Loader';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Report" element={<Report />} />
+        <Route path="/Loader" element={<Loader />} />
       </Routes>
     </Router>
   );
