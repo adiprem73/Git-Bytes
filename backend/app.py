a0e96de1fe8e038cd732f901122a17e7b0a06684
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-04-11 16:17:36 +0530

    added site-category endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
-
+from flask import request, jsonify
+from scanners import check_headers
+from utils import category
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello, World!"
-    
+
+@app.route("/header-scanner", methods=["POST"])
+def header_scanner():
+    url = request.json["url"]
+    result = check_headers(url)
+    print(result)
+    return jsonify(result)
+
+@app.route("/site-category", methods=["POST"])
+def site_category():
+    url = request.json["url"]
+    result = category(url)
+    print(result)
+    return jsonify(result)
+
+@app.route("/sql-injection-scanner", methods=["POST"])
+def sql_injection():
+    url = request.args.get("url")
+
+
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
